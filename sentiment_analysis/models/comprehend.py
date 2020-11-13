78501d721337eb993c7f2c3a4af4b7c9e7afd2a6
Gabe <wei.g.chu@gmail.com>
2020-11-13 11:53:50 +0800

    add comprehend model

---
+++ sentiment_analysis/models/comprehend.py
@@ -0,0 +1,20 @@
+from typing import Dict
+
+from decouple import config
+from sentiment_analysis.aws.config_session import config_cognito_session
+
+
+class Comprehend(object):
+    IDENTITY_POOL_ID = config("IDENTITY_POOL_ID")
+    AWS_REGION = "us-west-2"
+
+    def __init__(self):
+        comprehend = self._initiate_comprehend()
+        self.model_func = comprehend.detect_sentiment
+
+    def _initiate_comprehend(self):
+        sess = config_cognito_session(self.IDENTITY_POOL_ID, self.AWS_REGION)
+        return sess.client(service_name="comprehend", region_name=self.AWS_REGION)
+
+    def detect_sentiment(self, text: str, language_code: str) -> Dict:
+        return self.model_func(Text=text, LanguageCode=language_code)
