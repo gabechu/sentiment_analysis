296a238f58c5e6478822deb03d91669e60423100
Gabe <wei.g.chu@gmail.com>
2020-11-16 12:03:49 +0800

    rename file

--- sentiment_analysis/checklist/dump_predictions_pipeline.py
+++ sentiment_analysis/checklist/dump_predictions_pipeline.py
@@ -1,6 +1,7 @@
 from typing import Dict, List
 
 from sentiment_analysis.models.comprehend import Comprehend
+from sentiment_analysis.utils import get_clean_text
 from tqdm import tqdm
 
 from .convert_predictions import comprehend_to_checklist_predictions
@@ -10,11 +11,7 @@ def get_model() -> Comprehend:
     return Comprehend()
 
 
-def get_clean_text(text: str) -> str:
-    return text.rstrip()
-
-
-def load_test_file(file_path: str) -> List[str]:
+def load_tests(file_path: str) -> List[str]:
     with open(file_path) as f:
         raw_texts = f.readlines()
         clean_texts = list(map(lambda x: get_clean_text(x), raw_texts))
@@ -25,7 +22,7 @@ def make_prediction(text: str, model: Comprehend, language_code: str = "en"):
     return model.detect_sentiment(text, language_code)
 
 
-def dump_predictions_to_file(file_path: str, predictions: Dict):
+def dump_predictions(file_path: str, predictions: Dict):
     with open(file_path, "w") as f:
         for pred in predictions:
             str_pred = comprehend_to_checklist_predictions(pred)
@@ -33,15 +30,17 @@ def dump_predictions_to_file(file_path: str, predictions: Dict):
 
 
 def dump_predictions_pipeline():
+    # config
     test_text_file = "/Users/gabechu/Code/zendesk/sentiment_analysis/release_data/sentiment/tests_n500"
     dump_preds_file = "/Users/gabechu/Code/zendesk/sentiment_analysis/comprehend"
 
+    # steps
     model = get_model()
-    texts = load_test_file(test_text_file)
+    texts = load_tests(test_text_file)
 
     preds = [make_prediction(text, model) for text in tqdm(texts)]
 
-    dump_predictions_to_file(dump_preds_file, preds)
+    dump_predictions(dump_preds_file, preds)
 
 
 if __name__ == "__main__":
