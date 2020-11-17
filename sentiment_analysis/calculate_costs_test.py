d489e40f93c4a060c3a32278d6d0e051a6db7005
Gabe <wei.g.chu@gmail.com>
2020-11-17 14:38:05 +0800

    unittest calculate_comprehend_cost_no_batch

---
+++ sentiment_analysis/calculate_costs_test.py
@@ -0,0 +1,26 @@
+from .calculate_costs import calculate_comprehend_cost_no_batch
+from numpy.testing import assert_almost_equal
+
+
+def test_calculate_comprehend_cost_no_batch_single_text():
+    text = ["test"]
+    actual = calculate_comprehend_cost_no_batch(text)
+    assert_almost_equal(actual, 0.0003)
+
+
+def test_calculate_comprehend_cost_no_batch_less_than_3_unit_per_request():
+    texts = ["test", "test", "test"]
+    actual = calculate_comprehend_cost_no_batch(texts)
+    assert_almost_equal(actual, 0.0009)
+
+
+def test_calculate_comprehend_cost_no_batch_equal_to_3_unit_per_request():
+    texts = ["t" * 300]
+    actual = calculate_comprehend_cost_no_batch(texts)
+    assert_almost_equal(actual, 0.0003)
+
+
+def test_calculate_comprehend_cost_no_batch_greater_than_3_unit_per_request():
+    texts = ['t' * 550] * 10000
+    actual = calculate_comprehend_cost_no_batch(texts)
+    assert_almost_equal(actual, 6.)
