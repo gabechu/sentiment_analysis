33341d0acce42b0482f479814502aaaca0309e58
Gabe <wei.g.chu@gmail.com>
2020-11-24 15:07:44 +1100

    simplify implementations

--- sentiment_analysis/data_loaders/huggingface_loader.py
+++ sentiment_analysis/data_loaders/huggingface_loader.py
@@ -7,33 +7,8 @@ from pyarrow.lib import Table
 
 
 class HuggingfaceLoader(object):
-    # we are only interested in "label" and "text" for sentiment analysis
-    filter_mapping = {"sentiment140": ["text", "sentiment"]}
-
-    # Rename corresponding columns to literal label and text if they are not
-    rename_mapping = {
-        "allocine": {"review": "text"},
-        "sentiment140": {"sentiment": "label"},
-    }
-
     def __init__(self, dataset_name: str):
-        supported_datasets = [
-            "allocine",
-            "imdb",
-            "rotten_tomatoes",
-            "sentiment140",
-            "yelp_polarity",
-        ]
-        if dataset_name in supported_datasets:
-            self.dataset_name = dataset_name
-        else:
-            raise ValueError(
-                f"Dataset {dataset_name} not found, "
-                f"supports only {supported_datasets}."
-            )
-
-    def load_data(self):
-        self.data = load_dataset(self.dataset_name)
+        self.dataset_name = dataset_name
 
     def load_test_data(self) -> DataFrame:
         data: Union[DatasetDict, Dataset] = load_dataset(self.dataset_name)
@@ -42,15 +17,4 @@ class HuggingfaceLoader(object):
             raise KeyError(f"{self.dataset_name} does not have a test set.")
 
         test_data: Table = data["test"].data
-        df = test_data.to_pandas()
-
-        # filter columns
-        if self.dataset_name in self.filter_mapping:
-            df = df.filter(items=self.filter_mapping[self.dataset_name])
-
-        # rename columns
-        if self.dataset_name in self.rename_mapping:
-            df = df.rename(columns=self.rename_mapping[self.dataset_name])
-
-        # avoid randomness on column orders
-        return df.sort_index(axis=1)
+        return test_data.to_pandas()
--- sentiment_analysis/data_loaders/huggingface_loader_test.py
+++ sentiment_analysis/data_loaders/huggingface_loader_test.py
@@ -25,16 +25,6 @@ def test_huggingfaceloader_init():
     assert actual.dataset_name == "imdb"
 
 
-def test_huggingfaceloader_init_with_unsupported_dataset():
-    with raises(ValueError) as err:
-        HuggingfaceLoader("NotSupported")
-
-    assert str(err.value) == (
-        "Dataset NotSupported not found, supports only "
-        "['allocine', 'imdb', 'rotten_tomatoes', 'sentiment140', 'yelp_polarity']."
-    )
-
-
 @patch("sentiment_analysis.data_loaders.huggingface_loader.load_dataset")
 def test_huggingfaceloader_load_test_data_with_datasetdict(mock_load_dataset):
     mock_load_dataset.return_value = construct_dataset_dict(
@@ -61,37 +51,3 @@ def test_huggingfaceloader_load_test_data_without_testset(mock_load_dataset):
     # KeyError adds extra quotes to the error message
     # https://stackoverflow.com/questions/24998968/why-does-strkeyerror-add-extra-quotes
     assert str(err.value) == "'imdb does not have a test set.'"
-
-
-@patch("sentiment_analysis.data_loaders.huggingface_loader.load_dataset")
-def test_huggingfaceloader_load_test_data_with_filter_columns_for_sentiment140(
-    mock_load_dataset,
-):
-    mock_load_dataset.return_value = construct_dataset_dict(
-        {
-            "sentiment": [1, 0],
-            "text": ["good", "bad"],
-            "date": ["24/11/2020", "25/11/2020"],
-        }
-    )
-
-    loader = HuggingfaceLoader("sentiment140")
-    actual = loader.load_test_data()
-    expect = pd.DataFrame.from_dict({"label": [1, 0], "text": ["good", "bad"]})
-
-    assert actual.equals(expect.sort_index(axis=1))
-
-
-@patch("sentiment_analysis.data_loaders.huggingface_loader.load_dataset")
-def test_huggingfaceloader_load_test_data_with_rename_columns_for_allocine(
-    mock_load_dataset,
-):
-    mock_load_dataset.return_value = construct_dataset_dict(
-        {"label": [1, 0], "review": ["good", "bad"]}
-    )
-
-    loader = HuggingfaceLoader("allocine")
-    actual = loader.load_test_data()
-    expect = pd.DataFrame.from_dict({"label": [1, 0], "text": ["good", "bad"]})
-
-    assert actual.equals(expect.sort_index(axis=1))
