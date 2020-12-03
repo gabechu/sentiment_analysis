acb3da2ec016b3d879379f95035730a184167664
Gabe <wei.g.chu@gmail.com>
2020-12-03 15:22:41 +1100

    add responsemeta for comprehend labels

--- sentiment_analysis/labels/model_labels.py
+++ sentiment_analysis/labels/model_labels.py
@@ -9,11 +9,25 @@ class SentimentScore(TypedDict):
     Mixed: float
 
 
+HTTPHeaders = TypedDict(
+    "HTTPHeaders",
+    {"x-amzn-requestid": str, "content-type": str, "content-length": str, "date": str},
+)
+
+
+class ResponseMetadata(TypedDict):
+    RequestId: str
+    HTTPStatusCode: int
+    HTTPHeaders: HTTPHeaders
+    RetryAttempts: int
+
+
 class ComprehendResults(TypedDict):
     # Sentiment can have these 4 labels:
     # POSITIVE, NEGATIVE, NEUTRAL and MIXED
     Sentiment: str
     SentimentScore: SentimentScore
+    ResponseMetadata: ResponseMetadata
 
 
 class ComprehendLabel(Enum):
